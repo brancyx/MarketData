7a72e54217da04c166643c566d7d2601039ebb72
Charmaine Lim <charmainelim@Charmaines-MacBook-Pro.local>
2021-08-06 10:57:29 +0800

    refactor: init 'StockListViewModel' in 'StockListView'

--- MarketData/ContentView.swift
+++ MarketData/ContentView.swift
@@ -9,19 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-     @EnvironmentObject var vm: StockListViewModel
-    
     var body: some View {
-//        let vm = StockListViewModel(stockListService: MockStockListDataService())
         NavigationView {
-            if let stocks = vm.stocks {
-              StockListView(stocks: stocks)
-                .navigationTitle("Stock List")
-            } else {
-                ProgressView()
-                // TODO:
-                //Text("No Stocks")
-            }
+            StockListView()
+                .navigationTitle("Top Stocks")
         }
         
     }
--- MarketData/StockList/Views/StockListView.swift
+++ MarketData/StockList/Views/StockListView.swift
@@ -9,44 +9,46 @@ import SwiftUI
 
 struct StockListView: View {
     
-    public var stocks: [StockInfoModel]
+    @StateObject var vm = StockListViewModel(stockListService: MockStockListDataService())
     
     var body: some View {
-//        return List(stocks, rowContent: StockRow.init)
+        if let stocks = vm.stocks {
             List(stocks) { stock in
                 NavigationLink(destination: StockDetailsView(stock: stock)) {
                     StockRowView(stock: stock)
                 }
             }
-    }
-}
-
-struct StockList_Previews: PreviewProvider {
-
-    
-    static func loopThrough() -> [StockInfoModel] {
-        var stocks = [] as [StockInfoModel]
-        // parse json file
-        
-        for n in 1...5 {
-      // replace with for-loop through json file
-            let tradePrice = Amount(value: 650.00, currency: .usd)
-            let actualChange = Amount(value: 0.00, currency: .usd)
-            
-            let price = PriceInfo(symbol: "TSLA", tradePrice: tradePrice, actualChange: actualChange, percentageChange: 10.00)
-            let stock = StockInfoModel(name: "TESLA", symbol: "TSLA", imageURL: nil, priceInfo: price)
-            stocks.append(stock)
-        }
-        return stocks
-    }
-      
-    static var previews: some View {
-        let vm = StockListViewModel(stockListService: MockStockListDataService())
-        if let stocks = vm.stocks {
-            StockListView(stocks: stocks)
         } else {
-            Text("No Stocks")
+            ProgressView()
         }
-        
     }
 }
+
+//struct StockList_Previews: PreviewProvider {
+//
+//
+//    static func loopThrough() -> [StockInfoModel] {
+//        var stocks = [] as [StockInfoModel]
+//        // parse json file
+//
+//        for n in 1...5 {
+//      // replace with for-loop through json file
+//            let tradePrice = Amount(value: 650.00, currency: .usd)
+//            let actualChange = Amount(value: 0.00, currency: .usd)
+//
+//            let price = PriceInfo(symbol: "TSLA", tradePrice: tradePrice, actualChange: actualChange, percentageChange: 10.00)
+//            let stock = StockInfoModel(name: "TESLA", symbol: "TSLA", imageURL: nil, priceInfo: price)
+//            stocks.append(stock)
+//        }
+//        return stocks
+//    }
+//
+//    static var previews: some View {
+//        let vm = StockListViewModel(stockListService: MockStockListDataService())
+//        if let stocks = vm.stocks {
+//            StockListView(stocks: stocks)
+//        } else {
+//            Text("No Stocks")
+//        }
+//
+//    }
